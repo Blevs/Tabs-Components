785bb200486a9bec31e24ea434dac2aced1bb1ac
Henry Blevins <heblevi@gmail.com>
2019-03-07 15:50:54 -0500

    Image carousel

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,47 @@
+class Carousel {
+  constructor(carousel) {
+    this.element = carousel;
+    this.prev = document.createElement('span');
+    // next and previous buttons
+    this.prev.classList.add('prev');
+    this.prev.textContent = '❮';
+    this.next = document.createElement('span');
+    this.next.classList.add('next');
+    this.next.textContent = '❯';
+    this.element.appendChild(this.prev);
+    this.element.appendChild(this.next);
+    // images
+    this.images = this.element.querySelectorAll('img');
+    this.selected = 0;
+    this.images[this.selected].classList.add('show');
+    // indicators
+    const indicatorsSpan = document.createElement('span');
+    indicatorsSpan.classList.add('indicators');
+    this.indicators = [];
+    this.images.forEach(_ => {
+      const indicator = document.createElement('div');
+      indicator.classList.add('indicator');
+      indicatorsSpan.appendChild(indicator);
+      this.indicators.push(indicator);
+    });
+    this.element.appendChild(indicatorsSpan);
+    this.indicators[0].classList.add('show');
+    // scroll
+    this.next.addEventListener('click', () => {
+      this.images[this.selected].classList.remove('show');
+      this.indicators[this.selected].classList.remove('show');
+      this.selected = (this.selected + 1) % this.images.length;
+      this.images[this.selected].classList.add('show');
+      this.indicators[this.selected].classList.add('show');
+    });
+    this.prev.addEventListener('click', () => {
+      this.images[this.selected].classList.remove('show');
+      this.indicators[this.selected].classList.remove('show');
+      this.selected = (this.selected + this.images.length - 1) % this.images.length;
+      this.images[this.selected].classList.add('show');
+      this.indicators[this.selected].classList.add('show');
+    });
+  }
+}
+
+const carousel = new Carousel(document.querySelector('.carousel'));
